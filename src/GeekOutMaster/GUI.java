090bc7282547e1c33f834d240bec43241a8d51ea
MarlonAnacona <ejemplo@email.com>
2022-01-13 08:59:23 -0500

    Añadiendo

---
+++ src/GeekOutMaster/GUI.java
@@ -0,0 +1,16 @@
+package GeekOutMaster;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI(){
+    }
+
+
+    private  void intGUI(){
+        //create listener object or control object
+
+
+    }
+}
